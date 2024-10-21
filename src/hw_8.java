02d40af622f310e1a78cbd5a8867b7a1d667d79b
sapphir0v <6.sapphirov.6@bk.ru>
2024-10-22 00:26:41 +0300

    homework 8 #2 rework

--- src/hw_8.java
+++ src/hw_8.java
@@ -35,6 +35,6 @@ public class hw_8 {
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
-        } // как надо было это делать без гугла..
+        }
     }
 }
